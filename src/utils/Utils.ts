bda1859afa65ef753bee465fe39e45260fb15849
Jonas Mohr <jones.m.987654321@gmail.com>
2022-11-16 18:53:46 +0100

    Updated the which package, optimized the doesProgramExist function

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -6,16 +6,9 @@ import which from "which";
  * @returns true if it exists, false otherwise
  */
 export async function doesProgramExist(name: string): Promise<boolean> {
-	try {
-		await which(name);
-		return true;
-	} catch (error) {
-		return false;
-	}
-}
-
-export function clampFileName(maxLength: number, fileName: string): string {
-	if (fileName.length <= maxLength) return fileName;
-	return `${fileName.slice(undefined, maxLength - 3)}...`;
+	// @types/which not up-to-date, have to ignore this
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	return await which(name, { nothrow: true  }) != null;
 }
 

8aacaa2c78925b311a065cfa76321d89ab822ae4
Jonas Mohr <jones.m.987654321@gmail.com>
2022-12-14 19:11:17 +0100

    Fixed xml formatting

--- src/ocr/providers/NoOpOCRProvider.ts
+++ src/ocr/providers/NoOpOCRProvider.ts
@@ -15,8 +15,7 @@ export default class NoOpOCRProvider implements OCRProvider {
 
 	async performOCR(): Promise<Array<string> | undefined> {
 		// language=HTML
-		return [`
-			<?xml version="1.0" encoding="UTF-8"?>
+		return [`<?xml version="1.0" encoding="UTF-8"?>
 			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 				"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -27,8 +26,7 @@ export default class NoOpOCRProvider implements OCRProvider {
 			</head>
 			<body>
 			</body>
-			</html>
-		`];
+			</html>`];
 	}
 
 	async isUsable(): Promise<boolean> {
